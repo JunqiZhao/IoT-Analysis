faa41acb5a53eceeb65f7671464b1b20f5ec4076
Boyang Zhou <71305092+Boyang-Zhou@users.noreply.github.com>
2020-10-02 01:57:43 -0400

    Update counter_10min.py
    change counter to 1 hour
    add a plot function

--- Code/counter_10min.py
+++ Code/counter_10min.py
@@ -1,9 +1,28 @@
 import csv
-import time
 import datetime
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-with open('event_messages.csv','r') as csv_file:
+# bubble sort based on first element in nested list
+def sort_list(listt):
+    leng = len(listt)
+
+    for i in range(0, leng):
+
+        for j in range(0, leng-i-1):
+
+            if listt[j][0] > listt[j + 1][0]:
+
+                tempe = listt[j]
+                listt[j] = listt[j + 1]
+                listt[j + 1] = tempe
+
+    return listt
+
+
+with open('event_messages.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     next(csv_reader)
@@ -14,44 +33,31 @@ with open('event_messages.csv','r') as csv_file:
 
     # put EventTime to datetime format
     for line in list_shorted:
-
+        # transform it into datetime form and make sure it is zero-padded
         line[0] = datetime.datetime.strptime(line[0], "%m/%d/%Y %H:%M")
-        # Note: In documentation, it says the format of month must be zero-padded,
-        # but it seems to work in this non-zero-padded scenario
+        line[0] = line[0].strftime("%m/%d/%Y %H:%M")
+        line[0] = datetime.datetime.strptime(line[0], "%m/%d/%Y %H:%M")
+
+    # sort the list
+    list_sorted = sort_list(list_shorted)
 
     # divide into separate time slot
+
     stats = []
-    temp = list_shorted[0][0]
     count = 0
-    ten_minutes = datetime.timedelta(minutes=10)
     one_minutes = datetime.timedelta(minutes=1)
     one_hour = datetime.timedelta(hours=1)
-    for line in list_shorted:
-        if line == list_shorted[-1]:
-            if temp - ten_minutes < line[0] <= temp:
-                count += 1
-                stats.append([temp - ten_minutes + one_minutes, temp, count])
-            else:
-                stats.append([temp - ten_minutes + one_minutes, temp, count])
+    temp = list_sorted[0][0].replace(minute=00)-one_hour
 
-                if line[0] < temp - ten_minutes:
-                    temp = line[0]
-                else:
-                    temp = temp - ten_minutes
-                count = 1
-                stats.append([temp - ten_minutes + one_minutes, temp, count])
-        elif temp-ten_minutes < line[0] <= temp:
-
-            count += 1
-        else:
-
-            stats.append([temp-ten_minutes+one_minutes, temp, count])
-
-            if line[0] <= temp-ten_minutes:
-                temp = line[0]
-            else:
-                temp = temp-ten_minutes
-            count = 1
+    while temp < list_sorted[-1][0]:
+        for line in list_sorted:
+            if temp<line[0]<=temp+one_hour:
+                count+=1
+        stats.append([temp + one_minutes, temp+one_hour, count])
+        count=0
+        temp += one_hour
+    if stats[0][2] == 0:
+        stats.pop(0)
 
     # print out the result
     for line in stats:
@@ -60,6 +66,28 @@ with open('event_messages.csv','r') as csv_file:
         line[0] = line[0].strftime("%d/%m/%Y %H:%M")
         line[1] = line[1].strftime("%d/%m/%Y %H:%M")
 
-    stats = [["Start Time", "End Time", "Counts"]] + stats
+    time_interval=[]
+    counts=[]
+    for line in stats:
+        time_interval.append("{} to {}".format(line[0],line[1]))
+        counts.append(line[2])
 
-    print(stats)
+    time_interval_tuple=tuple(time_interval)
+
+
+
+    # draw the bar chart
+    objects = time_interval_tuple
+    y_pos = np.arange(len(objects))
+    performance = counts
+
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('counts')
+    plt.title('counter for 1h')
+
+    plt.show()
+
+    # stats = [["Start Time", "End Time", "Counts"]] + stats
+
+    # print(stats)
